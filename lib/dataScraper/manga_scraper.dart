9bfe0e494a5b03e486dac73f0a890434c8618d24
Mouaad Ouajib <mouaadouajib@gmail.com>
2023-08-07 15:40:53 +0100

    i added the default list of manga

--- lib/dataScraper/manga_scraper.dart
+++ lib/dataScraper/manga_scraper.dart
@@ -5,6 +5,7 @@ import 'package:mangago/models/manga.dart';
 class MangaScraper {
   final String baseUrl = "https://ww6.manganelo.tv";
 
+  //fetch search manga list
   Future<List<Manga>> fetchSearchList(String url) async {
     List<Manga> mangas = [];
 
@@ -32,6 +33,45 @@ class MangaScraper {
           title: title,
           mangaLink: mangaUrl,
           lastChapter: finalChapter,
+        ));
+      }
+    }
+
+    return mangas;
+  }
+
+  //fetch latest manga
+  Future<List<Manga>> latestManga(String url) async {
+    List<Manga> mangas = [];
+
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final document = parser.parse(response.body);
+      final mangaItems = document.querySelectorAll('.content-genres-item');
+
+      for (var mangaItem in mangaItems) {
+        final imgElement = mangaItem.querySelector('.genres-item-img img');
+        final imageUrl = imgElement?.attributes['src'] ?? '';
+
+        final titleElement = mangaItem.querySelector('.genres-item-name');
+        final title = titleElement?.text.trim() ?? '';
+
+        final mangaUrl = titleElement?.attributes['href'] ?? '';
+
+        final ratingElement = mangaItem.querySelector('.genres-item-rate');
+        final rating = ratingElement?.text.trim() ?? '';
+
+        final lastChapterElement = mangaItem.querySelector('.genres-item-chap');
+        final lastChapter = lastChapterElement?.text.trim() ?? '';
+        final lastChapterUrl = lastChapterElement?.attributes['href'] ?? '';
+
+        mangas.add(Manga(
+          coverImageUrl: imageUrl,
+          title: title,
+          mangaLink: mangaUrl,
+          rating: rating,
+          lastChapter: lastChapter,
+          lastChapterLink: lastChapterUrl,
         ));
       }
     }
